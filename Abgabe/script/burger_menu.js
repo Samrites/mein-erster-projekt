1588cfcd63b0ee23f351c6c70fe813c765e4f76e
Samrites <samrites2008@yahoo.com>
2025-07-23 12:08:23 +0200

    Mein erster Commit

---
+++ Abgabe/script/burger_menu.js
@@ -0,0 +1,9 @@
+function showBurgerMenu(){
+    const burger_menu = document.querySelector('.burger_menu');
+    burger_menu.style.display = 'flex'
+}
+
+function closeBurgerMenu(){
+    const burger_menu = document.querySelector('.burger_menu');
+    burger_menu.style.display = 'none'
+}
---
+++ Abgabe/script/servings_calculator.js
@@ -0,0 +1,12 @@
+/*
+const input = document.getElementById("servings");
+
+function multiply(ingredient, servings) {
+return ingredient * servings;
+}
+
+const ingredientresult = multiply(50, 3);
+console.log(ingredientresult);
+*/
+
+
